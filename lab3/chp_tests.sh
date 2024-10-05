4cc5db0f51635b44b866d9849b63ff91f5d3e01d
NukeWolf <smart.alex.huang@gmail.com>
2024-10-05 00:59:47 -0400

    chp tests done and cells done

---
+++ lab3/chp_tests.sh
@@ -0,0 +1,31 @@
+## Testing 10-place buffer
+
+echo "=========================== CHP TESTS "===========================
+echo ""
+echo "~~~~~~~~~~~~~~~~~ Buffer Test ~~~~~~~~~~~~~~~"
+
+actsim chp_tests.act -Wall:off "buf_test" < chptest.actscript
+
+
+# echo "~~~~~~~~~~~~~~~~~ Buffer Test ~~~~~~~~~~~~~~~"
+# actsim chp_tests.act -Wall:off "init_buf_test" < chptest.actscript
+echo ""
+echo "~~~~~~~~~~~~~~~~~ Source Test ~~~~~~~~~~~~~~~"
+actsim chp_tests.act -Wall:off "source_test" < chptest_setgnd.actscript
+echo ""
+echo "~~~~~~~~~~~~~~~~~ Sink Test ~~~~~~~~~~~~~~~"
+actsim chp_tests.act -Wall:off "sink_test" < chptest.actscript
+echo ""
+echo "~~~~~~~~~~~~~~~~~ Copy Test ~~~~~~~~~~~~~~~"
+actsim chp_tests.act -Wall:off "copy_test" < chptest.actscript
+echo ""
+echo "~~~~~~~~~~~~~~~~~ Split Test ~~~~~~~~~~~~~~~"
+actsim chp_tests.act -Wall:off "split_test" < chptest.actscript
+
+echo ""
+echo "~~~~~~~~~~~~~~~~~ Merge Test ~~~~~~~~~~~~~~~"
+actsim chp_tests.act -Wall:off "merge_test" < chptest.actscript
+
+echo ""
+echo "~~~~~~~~~~~~~~~~~ Adder Test ~~~~~~~~~~~~~~~"
+actsim chp_tests.act -Wall:off "adder_test" < chptest_setgnd.actscript | grep -E -i "Received|sending"
