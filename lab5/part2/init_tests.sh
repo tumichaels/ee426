f20554358a0a96bbc7572c32d7ea482f21060b87
Alex Huang <alex.huang@yale.edu>
2024-11-07 22:32:16 +0000

    set up all cell expansion

--- lab5/part2/init_tests.sh
+++ lab5/part2/init_tests.sh
@@ -11,7 +11,7 @@ for file in "$DIRECTORY"/*.rect; do
   if [[ -f "$file" ]]; then
     # Extract the base filename without the extension
     base_filename=$(basename "$file" .rect)
-    
+    act_cell=$(echo "$base_filename" | sed 's/_0_0//' | sed 's/_0_0/::/g')
     echo "Processing file: $file"
     echo "Base filename: $base_filename"
 
@@ -23,7 +23,7 @@ for file in "$DIRECTORY"/*.rect; do
     mkdir -p $base_filename/mag
     cp template/interact_script_template $base_filename/interact_script_template
     cp template/top-template.act $base_filename/top-template.act
-    sed -e "s/{{FILENAME}}/$base_filename/g" template/Makefile > $base_filename/Makefile
+    sed -e "s/{{FILENAME}}/$base_filename/g" -e "s/{{MAKE_CELL_PROC}}/$act_cell/g" template/Makefile > $base_filename/Makefile
 
     # Add your custom command here, using $file and $base_filename
     # Example: cat "$file"
