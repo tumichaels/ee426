69e7972ea0f869dce42a73296b03fbb6b716d07e
Alex Huang <alex.huang@yale.edu>
2024-11-08 05:19:17 +0000

    trying part 3

---
+++ lab5/part3/final_layout/_0_0std_0_0cells_0_0TIELOX1.tcl
@@ -0,0 +1,316 @@
+xload final_rect/_0_0std_0_0cells_0_0TIELOX1
+box 0 0 32 40
+label prboundary
+box 19 8 20 11
+paint ndiffusion
+box 20 9 20 9
+label "GND" right ndiffusion
+box 23 17 24 19
+paint polysilicon
+box 24 18 24 18
+label "x" right polysilicon
+box 23 19 28 20
+paint polysilicon
+box 24 20 24 20
+label "x" right polysilicon
+box 19 25 20 28
+paint pdiffusion
+box 20 26 20 26
+label "x" right pdiffusion
+box 16 8 19 11
+paint ndiffusion
+box 17 9 17 9
+label "GND" right ndiffusion
+box 27 16 28 19
+paint polysilicon
+box 28 17 28 17
+label "x" right polysilicon
+box 16 25 19 28
+paint pdiffusion
+box 17 26 17 26
+label "x" right pdiffusion
+box 15 6 20 8
+paint ndiffusion
+box 16 7 16 7
+label "GND" right ndiffusion
+box 15 8 16 11
+paint ndiffusion
+box 16 9 16 9
+label "GND" right ndiffusion
+box 15 11 20 12
+paint ndiffusion
+box 16 12 16 12
+label "GND" right ndiffusion
+box 12 7 13 10
+paint ndiffusion
+box 13 8 13 8
+label "Y" right ndiffusion
+box 24 16 27 19
+paint polysilicon
+box 25 17 25 17
+label "x" right polysilicon
+box 15 19 20 25
+paint pdiffusion
+box 16 20 16 20
+label "x" right pdiffusion
+box 15 25 16 28
+paint pdiffusion
+box 16 26 16 26
+label "x" right pdiffusion
+box 15 28 20 29
+paint pdiffusion
+box 16 29 16 29
+label "x" right pdiffusion
+box 12 25 13 28
+paint pdiffusion
+box 13 26 13 26
+label "Vdd" right pdiffusion
+box 13 4 15 6
+paint polysilicon
+box 14 5 14 5
+label "x" right polysilicon
+box 13 6 15 12
+paint ntransistor
+box 14 7 14 7
+label "x" right ntransistor
+box 9 7 12 10
+paint ndiffusion
+box 10 8 10 8
+label "Y" right ndiffusion
+box 13 12 15 15
+paint polysilicon
+box 14 13 14 13
+label "x" right polysilicon
+box 13 15 28 16
+paint polysilicon
+box 14 16 14 16
+label "x" right polysilicon
+box 13 16 24 17
+paint polysilicon
+box 14 17 14 17
+label "x" right polysilicon
+box 13 17 15 19
+paint polysilicon
+box 14 18 14 18
+label "x" right polysilicon
+box 13 19 15 29
+paint ptransistor
+box 14 20 14 20
+label "x" right ptransistor
+box 9 25 12 28
+paint pdiffusion
+box 10 26 10 26
+label "Vdd" right pdiffusion
+box 13 29 15 31
+paint polysilicon
+box 14 30 14 30
+label "x" right polysilicon
+box 8 6 13 7
+paint ndiffusion
+box 9 7 9 7
+label "Y" right ndiffusion
+box 8 7 9 10
+paint ndiffusion
+box 9 8 9 8
+label "Y" right ndiffusion
+box 8 10 13 12
+paint ndiffusion
+box 9 11 9 11
+label "Y" right ndiffusion
+box 8 19 13 25
+paint pdiffusion
+box 9 20 9 20
+label "Vdd" right pdiffusion
+box 8 25 9 28
+paint pdiffusion
+box 9 26 9 26
+label "Vdd" right pdiffusion
+box 8 28 13 29
+paint pdiffusion
+box 9 29 9 29
+label "Vdd" right pdiffusion
+box 24 16 27 19
+paint pc
+box 25 17 25 17
+label "x" right pc
+box 16 8 19 11
+paint ndc
+box 17 9 17 9
+label "GND" right ndc
+box 16 25 19 28
+paint pdc
+box 17 26 17 26
+label "x" right pdc
+box 9 7 12 10
+paint ndc
+box 10 8 10 8
+label "Y" right ndc
+box 9 25 12 28
+paint pdc
+box 10 26 10 26
+label "Vdd" right pdc
+box 27 16 28 19
+paint m1
+box 28 17 28 17
+label "x" right m1
+box 27 32 28 35
+paint m1
+box 28 33 28 33
+label "GND" right m1
+box 24 16 27 19
+paint m1
+box 25 17 25 17
+label "x" right m1
+box 24 20 27 25
+paint m1
+box 25 21 25 21
+label "x" right m1
+box 24 32 27 35
+paint m1
+box 25 33 25 33
+label "GND" right m1
+box 24 8 27 11
+paint m1
+box 25 9 25 9
+label "GND" right m1
+box 23 15 28 16
+paint m1
+box 24 16 24 16
+label "x" right m1
+box 23 16 24 19
+paint m1
+box 24 17 24 17
+label "x" right m1
+box 23 19 28 20
+paint m1
+box 24 20 24 20
+label "x" right m1
+box 23 31 28 32
+paint m1
+box 24 32 24 32
+label "GND" right m1
+box 23 32 24 35
+paint m1
+box 24 33 24 33
+label "GND" right m1
+box 23 35 28 36
+paint m1
+box 24 36 24 36
+label "GND" right m1
+box 19 8 24 11
+paint m1
+box 20 9 20 9
+label "GND" right m1
+box 19 25 27 28
+paint m1
+box 20 26 20 26
+label "x" right m1
+box 16 8 19 11
+paint m1
+box 17 9 17 9
+label "GND" right m1
+box 16 25 19 28
+paint m1
+box 17 26 17 26
+label "x" right m1
+box 15 8 16 11
+paint m1
+box 16 9 16 9
+label "GND" right m1
+box 15 25 16 28
+paint m1
+box 16 26 16 26
+label "x" right m1
+box 9 7 12 10
+paint m1
+box 10 8 10 8
+label "Y" right m1
+port class output
+port make
+box 9 10 12 11
+paint m1
+box 10 11 10 11
+label "Y" right m1
+port class output
+port make
+box 9 24 12 25
+paint m1
+box 10 25 10 25
+label "Vdd" right m1
+box 9 25 12 28
+paint m1
+box 10 26 10 26
+label "Vdd" right m1
+box 9 28 12 32
+paint m1
+box 10 29 10 29
+label "Vdd" right m1
+box 8 4 12 7
+paint m1
+box 9 5 9 5
+label "Y" right m1
+port class output
+port make
+box 8 7 9 8
+paint m1
+box 9 8 9 8
+label "Y" right m1
+port class output
+port make
+box 8 32 12 36
+paint m1
+box 9 33 9 33
+label "Vdd" right m1
+box 24 8 27 11
+paint mcon
+box 25 9 25 9
+label "GND" right mcon
+box 24 32 27 35
+paint mcon
+box 25 33 25 33
+label "GND" right mcon
+box 27 8 28 11
+paint m2
+box 28 9 28 9
+label "GND" right m2
+box 27 32 28 35
+paint m2
+box 28 33 28 33
+label "GND" right m2
+box 24 8 27 11
+paint m2
+box 25 9 25 9
+label "GND" right m2
+box 24 12 27 31
+paint m2
+box 25 13 25 13
+label "GND" right m2
+box 24 32 27 35
+paint m2
+box 25 33 25 33
+label "GND" right m2
+box 23 7 28 8
+paint m2
+box 24 8 24 8
+label "GND" right m2
+box 23 8 24 11
+paint m2
+box 24 9 24 9
+label "GND" right m2
+box 23 11 28 12
+paint m2
+box 24 12 24 12
+label "GND" right m2
+box 23 31 28 32
+paint m2
+box 24 32 24 32
+label "GND" right m2
+box 23 32 24 35
+paint m2
+box 24 33 24 33
+label "GND" right m2
+box 23 35 28 36
+paint m2
+box 24 36 24 36
+label "GND" right m2
+proc lcell { x } { load "_0_0cell_0_0g${x}x0" }
---
+++ lab5/part3/mag/_0_0std_0_0cells_0_0FAX1.tcl
@@ -0,0 +1,400 @@
+xload init_rect/_0_0std_0_0cells_0_0FAX1
+box 103 31 108 39
+paint pdiffusion
+box 104 32 104 32
+label "YC" right pdiffusion
+box 101 31 103 39
+paint ptransistor
+box 101 39 103 41
+paint polysilicon
+box 100 31 101 39
+paint pdiffusion
+box 96 31 100 79
+paint pdiffusion
+box 97 32 97 32
+label "Vdd" right pdiffusion
+box 94 31 96 79
+paint ptransistor
+box 94 79 96 81
+paint polysilicon
+box 101 24 103 26
+paint polysilicon
+box 102 25 102 25
+label "_YC" right polysilicon
+box 101 29 103 31
+paint polysilicon
+box 102 30 102 30
+label "_YC" right polysilicon
+box 91 31 94 79
+paint pdiffusion
+box 89 31 91 79
+paint ptransistor
+box 89 79 91 81
+paint polysilicon
+box 86 31 89 79
+paint pdiffusion
+box 94 24 96 26
+paint polysilicon
+box 95 25 95 25
+label "A" right polysilicon
+box 94 29 96 31
+paint polysilicon
+box 95 30 95 30
+label "A" right polysilicon
+box 84 31 86 79
+paint ptransistor
+box 84 79 86 81
+paint polysilicon
+box 80 31 84 79
+paint pdiffusion
+box 89 24 91 26
+paint polysilicon
+box 90 25 90 25
+label "B" right polysilicon
+box 89 29 91 31
+paint polysilicon
+box 90 30 90 30
+label "B" right polysilicon
+box 77 31 80 67
+paint pdiffusion
+box 78 32 78 32
+label "_YS" right pdiffusion
+box 75 31 77 67
+paint ptransistor
+box 75 67 77 69
+paint polysilicon
+box 84 24 86 26
+paint polysilicon
+box 85 25 85 25
+label "C" right polysilicon
+box 84 29 86 31
+paint polysilicon
+box 85 30 85 30
+label "C" right polysilicon
+box 74 31 75 67
+paint pdiffusion
+box 103 18 108 24
+paint ndiffusion
+box 104 19 104 19
+label "YC" right ndiffusion
+box 70 31 74 71
+paint pdiffusion
+box 71 32 71 32
+label "#12" right pdiffusion
+box 101 16 103 18
+paint polysilicon
+box 101 18 103 24
+paint ntransistor
+box 75 24 77 26
+paint polysilicon
+box 76 25 76 25
+label "_YC" right polysilicon
+box 75 29 77 31
+paint polysilicon
+box 76 30 76 30
+label "_YC" right polysilicon
+box 68 31 70 71
+paint ptransistor
+box 68 71 70 73
+paint polysilicon
+box 100 18 101 24
+paint ndiffusion
+box 64 31 68 71
+paint pdiffusion
+box 96 4 100 24
+paint ndiffusion
+box 97 5 97 5
+label "GND" right ndiffusion
+box 68 24 70 26
+paint polysilicon
+box 69 25 69 25
+label "A" right polysilicon
+box 68 29 70 31
+paint polysilicon
+box 69 30 69 30
+label "A" right polysilicon
+box 61 31 64 67
+paint pdiffusion
+box 62 32 62 32
+label "Vdd" right pdiffusion
+box 94 2 96 4
+paint polysilicon
+box 94 4 96 24
+paint ntransistor
+box 59 31 61 67
+paint ptransistor
+box 59 67 61 69
+paint polysilicon
+box 91 4 94 24
+paint ndiffusion
+box 59 24 61 26
+paint polysilicon
+box 60 25 60 25
+label "C" right polysilicon
+box 59 29 61 31
+paint polysilicon
+box 60 30 60 30
+label "C" right polysilicon
+box 54 31 59 67
+paint pdiffusion
+box 55 32 55 32
+label "#12" right pdiffusion
+box 89 2 91 4
+paint polysilicon
+box 89 4 91 24
+paint ntransistor
+box 52 31 54 67
+paint ptransistor
+box 52 67 54 69
+paint polysilicon
+box 86 4 89 24
+paint ndiffusion
+box 52 24 54 26
+paint polysilicon
+box 53 25 53 25
+label "B" right polysilicon
+box 52 29 54 31
+paint polysilicon
+box 53 30 53 30
+label "B" right polysilicon
+box 48 31 52 67
+paint pdiffusion
+box 84 2 86 4
+paint polysilicon
+box 84 4 86 24
+paint ntransistor
+box 45 31 48 54
+paint pdiffusion
+box 46 32 46 32
+label "Vdd" right pdiffusion
+box 77 4 84 24
+paint ndiffusion
+box 78 5 78 5
+label "_YS" right ndiffusion
+box 45 14 48 24
+paint ndiffusion
+box 46 15 46 15
+label "GND" right ndiffusion
+box 43 31 45 54
+paint ptransistor
+box 43 54 45 56
+paint polysilicon
+box 75 2 77 4
+paint polysilicon
+box 75 4 77 24
+paint ntransistor
+box 43 12 45 14
+paint polysilicon
+box 43 14 45 24
+paint ntransistor
+box 43 24 45 26
+paint polysilicon
+box 44 25 44 25
+label "A" right polysilicon
+box 43 29 45 31
+paint polysilicon
+box 44 30 44 30
+label "A" right polysilicon
+box 38 31 43 54
+paint pdiffusion
+box 70 4 75 24
+paint ndiffusion
+box 71 5 71 5
+label "#15" right ndiffusion
+box 38 14 43 24
+paint ndiffusion
+box 39 15 39 15
+label "#3" right ndiffusion
+box 36 31 38 54
+paint ptransistor
+box 36 54 38 56
+paint polysilicon
+box 68 2 70 4
+paint polysilicon
+box 68 4 70 24
+paint ntransistor
+box 36 12 38 14
+paint polysilicon
+box 36 14 38 24
+paint ntransistor
+box 36 24 38 26
+paint polysilicon
+box 37 25 37 25
+label "B" right polysilicon
+box 36 29 38 31
+paint polysilicon
+box 37 30 37 30
+label "B" right polysilicon
+box 31 31 36 54
+paint pdiffusion
+box 32 32 32 32
+label "#8" right pdiffusion
+box 61 4 68 24
+paint ndiffusion
+box 62 5 62 5
+label "GND" right ndiffusion
+box 31 14 36 24
+paint ndiffusion
+box 32 15 32 15
+label "GND" right ndiffusion
+box 29 31 31 54
+paint ptransistor
+box 29 54 31 56
+paint polysilicon
+box 59 2 61 4
+paint polysilicon
+box 59 4 61 24
+paint ntransistor
+box 29 12 31 14
+paint polysilicon
+box 29 14 31 24
+paint ntransistor
+box 29 24 31 26
+paint polysilicon
+box 30 25 30 25
+label "A" right polysilicon
+box 29 29 31 31
+paint polysilicon
+box 30 30 30 30
+label "A" right polysilicon
+box 24 31 29 54
+paint pdiffusion
+box 25 32 25 32
+label "_YC" right pdiffusion
+box 54 4 59 24
+paint ndiffusion
+box 55 5 55 5
+label "#15" right ndiffusion
+box 24 14 29 24
+paint ndiffusion
+box 22 31 24 54
+paint ptransistor
+box 22 54 24 56
+paint polysilicon
+box 52 2 54 4
+paint polysilicon
+box 52 4 54 24
+paint ntransistor
+box 22 12 24 14
+paint polysilicon
+box 22 14 24 24
+paint ntransistor
+box 22 24 24 26
+paint polysilicon
+box 23 25 23 25
+label "B" right polysilicon
+box 22 29 24 31
+paint polysilicon
+box 23 30 23 30
+label "B" right polysilicon
+box 18 31 22 54
+paint pdiffusion
+box 48 4 52 24
+paint ndiffusion
+box 15 14 22 24
+paint ndiffusion
+box 16 15 16 15
+label "_YC" right ndiffusion
+box 15 31 18 42
+paint pdiffusion
+box 16 32 16 32
+label "#8" right pdiffusion
+box 13 12 15 14
+paint polysilicon
+box 13 14 15 24
+paint ntransistor
+box 13 24 15 26
+paint polysilicon
+box 14 25 14 25
+label "C" right polysilicon
+box 13 29 15 31
+paint polysilicon
+box 14 30 14 30
+label "C" right polysilicon
+box 13 31 15 42
+paint ptransistor
+box 13 42 15 44
+paint polysilicon
+box 8 14 13 24
+paint ndiffusion
+box 9 15 9 15
+label "#3" right ndiffusion
+box 8 31 13 42
+paint pdiffusion
+box 9 32 9 32
+label "Vdd" right pdiffusion
+box 121 18 126 24
+paint ndiffusion
+box 122 19 122 19
+label "YS" right ndiffusion
+box 121 31 126 39
+paint pdiffusion
+box 122 32 122 32
+label "Vdd" right pdiffusion
+box 119 16 121 18
+paint polysilicon
+box 119 18 121 24
+paint ntransistor
+box 119 24 121 26
+paint polysilicon
+box 120 25 120 25
+label "_YS" right polysilicon
+box 119 29 121 31
+paint polysilicon
+box 120 30 120 30
+label "_YS" right polysilicon
+box 119 31 121 39
+paint ptransistor
+box 119 39 121 41
+paint polysilicon
+box 114 18 119 24
+paint ndiffusion
+box 115 19 115 19
+label "GND" right ndiffusion
+box 114 31 119 39
+paint pdiffusion
+box 115 32 115 32
+label "YS" right pdiffusion
+box 136 80 140 84
+paint m1
+box 137 81 137 81
+label "GND" right m1
+port class input
+port make
+box 104 80 108 84
+paint m1
+box 105 81 105 81
+label "Vdd" right m1
+port class input
+port make
+box 72 80 76 84
+paint m1
+box 73 81 73 81
+label "C" right m1
+port class input
+port make
+box 72 4 76 8
+paint m1
+box 73 5 73 5
+label "YS" right m1
+port class output
+port make
+box 40 80 44 84
+paint m1
+box 41 81 41 81
+label "B" right m1
+port class input
+port make
+box 8 4 12 8
+paint m1
+box 9 5 9 5
+label "YC" right m1
+port class output
+port make
+box 8 80 12 84
+paint m1
+box 9 81 9 81
+label "A" right m1
+port class input
+port make
+proc lcell { x } { load "_0_0cell_0_0g${x}x0" }
