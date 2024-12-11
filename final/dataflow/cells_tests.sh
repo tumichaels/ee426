11ad12b9d9f297844f472f42298563da747ac832
NukeWolf <smart.alex.huang@gmail.com>
2024-12-11 18:28:16 -0500

    Everything is N-bit

--- final/dataflow/cells_tests.sh
+++ final/dataflow/cells_tests.sh
@@ -4,28 +4,27 @@ echo "=========================== CHP TESTS "===========================
 echo ""
 echo "~~~~~~~~~~~~~~~~~ Buffer Test ~~~~~~~~~~~~~~~"
 
-actsim chp_tests.act -Wall:off "buf_test" < chptest.actscript
-
+actsim cells_tests.act -Wall:off "buf_test" < chptest.actscript
 
 echo "~~~~~~~~~~~~~~~~~ Init_Buffer Test ~~~~~~~~~~~~~~~"
-actsim chp_tests.act -Wall:off "init_buf_test" < chptest.actscript
+actsim cells_tests.act -Wall:off "init_buf_test" < chptest.actscript
 echo ""
 echo "~~~~~~~~~~~~~~~~~ Source Test ~~~~~~~~~~~~~~~"
-actsim chp_tests.act -Wall:off "source_test" < chptest.actscript
+actsim cells_tests.act -Wall:off "source_test" < chptest.actscript
 echo ""
 echo "~~~~~~~~~~~~~~~~~ Sink Test ~~~~~~~~~~~~~~~"
-actsim chp_tests.act -Wall:off "sink_test" < chptest.actscript
+actsim cells_tests.act -Wall:off "sink_test" < chptest.actscript
 echo ""
 echo "~~~~~~~~~~~~~~~~~ Copy Test ~~~~~~~~~~~~~~~"
-actsim chp_tests.act -Wall:off "copy_test" < chptest.actscript
+actsim cells_tests.act -Wall:off "copy_test" < chptest.actscript
 echo ""
 echo "~~~~~~~~~~~~~~~~~ Split Test ~~~~~~~~~~~~~~~"
-actsim chp_tests.act -Wall:off "split_test" < chptest.actscript
+actsim cells_tests.act -Wall:off "split_test" < chptest.actscript
 
 echo ""
 echo "~~~~~~~~~~~~~~~~~ Merge Test ~~~~~~~~~~~~~~~"
-actsim chp_tests.act -Wall:off "merge_test" < chptest.actscript
+actsim cells_tests.act -Wall:off "merge_test" < chptest.actscript
 
 echo ""
 echo "~~~~~~~~~~~~~~~~~ Adder Test ~~~~~~~~~~~~~~~"
-actsim chp_tests.act -Wall:off "adder_test" < chptest.actscript | grep -E -i "Received|sending"
+actsim cells_tests.act -Wall:off "adder_test" < chptest.actscript | grep -E -i "Received|sending"
